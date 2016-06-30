64f29ab7c204ed1643ac7ba7cd6b407556d7612a
Николай Кузьменко < >
2016-06-30 21:16:39 +0300

    update at_to_cart

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 
 function something()
-	{
+{
 
 		var x = window.localStorage.getItem('bbb');
 
@@ -9,11 +9,16 @@ function something()
 		window.localStorage.setItem('bbb', x);
 
 		alert(x);
-	
-	}
+}
 
 
-function add_to_cart()
+function add_to_cart(id)
 {
-	
+	var key = 'product_' + id;
+	var x = window.localStorage.getItem(key)
+
+	x = x * 1 + 1;
+
+	window.localStorage.setItem(key, x);
+
 }
