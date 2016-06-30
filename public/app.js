749b0a5bb0e5280e6b7b52d38db2a9395efbaed2
Николай Кузьменко < >
2016-06-30 21:47:41 +0300

    add summary pizzas in cart

--- public/app.js
+++ public/app.js
@@ -15,10 +15,16 @@ function something()
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
+	var count = 0;
 	var x = window.localStorage.getItem(key)
-
 	x = x * 1 + 1;
-
 	window.localStorage.setItem(key, x);
-
+	for(var i=0, len=localStorage.length; i<len; i++) 
+	{
+    	var key = localStorage.key(i);
+    	var value = localStorage[key];
+    	count = count * 1 + value * 1;
+    	
+	}
+	console.log(count);
 }
