33b6833917e0eb91396dc8455e2738e7a0e2d528
Николай Кузьменко < >
2016-06-30 20:06:44 +0300

    display products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630150853) do
+ActiveRecord::Schema.define(version: 20160630152935) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
