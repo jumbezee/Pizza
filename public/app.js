13bb214f363e58bccdfb90e1140b270e8f1323b0
Николай Кузьменко < >
2016-06-30 23:30:52 +0300

    Update orders button

--- public/app.js
+++ public/app.js
@@ -21,6 +21,7 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 
 	update_orders_input();
+	update_orders_button();
 }
 
 function update_orders_input()
@@ -29,6 +30,11 @@ function update_orders_input()
 	$('#orders_input').val(orders);
 }
 
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
+}
 
 function cart_get_number_of_items()
 {
