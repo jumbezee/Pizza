7ff947cb28708221c36c165f69df7d88c4c7df8e
Nikolay <kuz-kol@mail.ru>
2016-07-01 03:08:27 +0300

    add new model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630152935) do
+ActiveRecord::Schema.define(version: 20160701000047) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text     "name"
+    t.text     "phone"
+    t.text     "adress"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
